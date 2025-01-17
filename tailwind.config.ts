126bced6631e5b5d776caf950a5abace71f9610d
Justin <justinguerrerowork@gmail.com>
2025-01-17 18:59:54 -0500

    hero and button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import fluid, { extract } from 'fluid-tailwind'
+import fluid, { extract, screens, fontSize, FluidThemeConfig } from 'fluid-tailwind'
 
 
 export default {
@@ -11,11 +11,25 @@ export default {
   ],
   extract
 },
-  theme: {
+theme: {
+  fluid: (({ theme }) => ({
+    defaultScreens: ["20rem", theme("screens.lg")],
+  })) satisfies FluidThemeConfig,
+  screens,
+  fontSize,
     extend: {
       fontFamily: {
         sans: ["var(--font-bowlby-sc)"],
         mono: ["var(--font-dm-mono)"],
+      },
+      colors: {
+        "brand-blue": "#4876ff",
+        "brand-lime": "#d9f154",
+        "brand-navy": "#2e3192",
+        "brand-orange": "#ff7347",
+        "brand-pink": "#f7d0e9",
+        "brand-purple": "#692e54",
+        "brand-gray": "#fffdf9",
       },
     },
   },
